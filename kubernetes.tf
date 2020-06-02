56eff1c67609a460ae5aa5f4eda9c298ae06d95a
Steffen F. Qvistgaard <steffen@sublife.dk>
2020-06-02 15:12:02 +0200

    fix: Added token argument to kubectl command

--- kubernetes.tf
+++ kubernetes.tf
@@ -12,7 +12,7 @@ resource "null_resource" "aws_vpc_cni" {
     environment = {
       KUBECONFIG = module.eks.kubeconfig_filename
     }
-    command = "kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/master/config/v${var.cluster_aws_cni_version}/aws-k8s-cni.yaml"
+    command = "kubectl --token=${data.aws_eks_cluster_auth.cluster.token} apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/master/config/v${var.cluster_aws_cni_version}/aws-k8s-cni.yaml"
   }
 }
 
