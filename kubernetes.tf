7ef7bdd44e6f68f8052a1bb109f7309a920151d3
Steffen F. Qvistgaard <steffen@sublife.dk>
2020-06-04 14:20:02 +0200

    fix: removed unneeded cni configuration

--- kubernetes.tf
+++ kubernetes.tf
@@ -7,12 +7,3 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
-resource "null_resource" "aws_vpc_cni" {
-  provisioner "local-exec" {
-    environment = {
-      KUBECONFIG = module.eks.kubeconfig_filename
-    }
-    command = "kubectl --token=${data.aws_eks_cluster_auth.cluster.token} apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/master/config/v${var.cluster_aws_cni_version}/aws-k8s-cni.yaml"
-  }
-}
-
