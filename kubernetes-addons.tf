1be0fc183fa10eb45513ffb80750f4fd1dc803cf
Steffen F. Qvistgaard <folman@nuuday.dk>
2020-06-08 15:54:23 +0200

    fix: Fixed module version

--- kubernetes-addons.tf
+++ kubernetes-addons.tf
@@ -1,15 +1,14 @@
 module "cilium" {
   # TODO: Add extra configuration variables
   # source = "../terraform-aws-eks-addons/modules/cilium"
-  source       = "github.com/nuuday/terraform-aws-eks-addons//modules/cilium?ref=adding-new-modules"
+  source       = "github.com/nuuday/terraform-aws-eks-addons//modules/cilium?ref=v0.2.0"
   cluster_name = var.cluster_name
   enable       = var.cilium_enable
 }
 
 module "loki" {
   # TODO: Add extra configuration variables
-  source = "../terraform-aws-eks-addons/modules/loki"
-  # source = "github.com/nuuday/terraform-aws-eks-addons//modules/loki?ref=adding-new-modules"
+  source                   = "github.com/nuuday/terraform-aws-eks-addons//modules/loki?ref=loki-s3-dynamodb-storage"
   enable                   = var.loki_enable
   cluster_name             = var.cluster_name
   oidc_provider_issuer_url = module.eks.cluster_oidc_issuer_url
@@ -19,7 +18,7 @@ module "loki" {
 module "prometheus" {
   # TODO: Add extra configuration variables
   # source = "../terraform-aws-eks-addons/modules/prometheus"
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/prometheus?ref=adding-new-modules"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/prometheus?ref=v0.2.0"
   enable = var.prometheus_enable
 }
 
@@ -48,19 +47,19 @@ module "cluster-autoscaler" {
 
 
 module "kube-monkey" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/kube-monkey?ref=adding-new-modules"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/kube-monkey?ref=v0.2.0"
   # source = "../terraform-aws-eks-addons/modules/kube-monkey"
   enable = var.kube_monkey_enable
 }
 
 module "metrics-server" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/metrics-server?ref=adding-new-modules"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/metrics-server?ref=v0.2.0"
   # source = "../terraform-aws-eks-addons/modules/metrics-server"
   enable = var.metrics_server_enable
 }
 
 module "aws-node-termination-handler" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/aws-node-termination-handler?ref=adding-new-modules"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/aws-node-termination-handler?ref=v0.2.0"
   # source = "../terraform-aws-eks-addons/modules/aws-node-termination-handler"
   enable = var.aws_node_termination_handler_enable
 }
