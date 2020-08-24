0687bf59ef39b7d4ecbca9f383e662f1b9efe2ed
folman@tdc.dk <folman@nuuday.dk>
2020-08-24 11:07:28 +0200

    feat: Updated all addons to newest version

--- kubernetes-addons.tf
+++ kubernetes-addons.tf
@@ -1,5 +1,5 @@
 module "cluster-autoscaler" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/cluster-autoscaler?ref=0.3.1"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/cluster-autoscaler?ref=v0.9.9"
 
   enable                   = var.cluster_autoscaler_enable
   cluster_name             = var.cluster_name
@@ -30,7 +30,7 @@ module "prometheus" {
 }
 
 module "external-dns" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/external-dns?ref=0.5.0"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/external-dns?ref=v0.9.9"
   # source                   = "../terraform-aws-eks-addons/modules/external-dns"
   enable                   = var.external_dns_enable
   oidc_provider_issuer_url = module.eks.cluster_oidc_issuer_url
@@ -39,7 +39,7 @@ module "external-dns" {
 }
 
 module "cert-manager" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/cert-manager?ref=0.5.0"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/cert-manager?ref=v0.9.9"
   # source                   = "../terraform-aws-eks-addons/modules/cert-manager"
   enable                   = var.cert_manager_enable
   email                    = var.cert_manager_email
@@ -49,15 +49,16 @@ module "cert-manager" {
   oidc_provider_issuer_url = module.eks.cluster_oidc_issuer_url
   route53_zones            = var.route53_zones
   tags                     = var.tags
+  kubectl_server = data.aws_eks_cluster.cluster.endpoint
 }
 
 module "kube-monkey" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/kube-monkey?ref=0.5.0"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/kube-monkey?ref=v0.9.9"
   enable = var.kube_monkey_enable
 }
 
 module "ingress_controller_nginx" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/nginx-ingress-controller?ref=0.5.0"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/nginx-ingress-controller?ref=v0.9.9"
   # source            = "../terraform-aws-eks-addons/modules/nginx-ingress-controller"
   enable            = var.ingress_controller_ingress_enable && var.ingress_controller_ingress_flavour == "nginx"
   loadbalancer_fqdn = module.lb.this_lb_dns_name
@@ -73,11 +74,11 @@ module "ingress_controller_nginx" {
 }
 
 module "metrics-server" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/metrics-server?ref=0.5.0"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/metrics-server?ref=v0.9.9"
   enable = var.metrics_server_enable
 }
 
 module "aws-node-termination-handler" {
-  source = "github.com/nuuday/terraform-aws-eks-addons//modules/aws-node-termination-handler?ref=0.5.0"
+  source = "github.com/nuuday/terraform-aws-eks-addons//modules/aws-node-termination-handler?ref=v0.9.9"
   enable = var.aws_node_termination_handler_enable
 }
