5b9a2cb930b14ba2a90cb50406837ea6c0932b4c
folman@tdc.dk <folman@nuuday.dk>
2020-08-24 11:14:15 +0200

    fix: Fixed cert-manager variable, removed variable that's not used anymore

--- kubernetes-addons.tf
+++ kubernetes-addons.tf
@@ -44,12 +44,11 @@ module "cert-manager" {
   enable                   = var.cert_manager_enable
   email                    = var.cert_manager_email
   ingress_class            = local.cert_manager_ingress_class
-  kubeconfig_filename      = module.eks.kubeconfig_filename
   kubectl_token            = data.aws_eks_cluster_auth.cluster.token
   oidc_provider_issuer_url = module.eks.cluster_oidc_issuer_url
   route53_zones            = var.route53_zones
   tags                     = var.tags
-  kubectl_server = data.aws_eks_cluster.cluster.endpoint
+  kubectl_server           = data.aws_eks_cluster.cluster.endpoint
 }
 
 module "kube-monkey" {
